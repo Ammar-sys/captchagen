1dddb6d37ab7c7c263dd586f31213438671277f0
Ammar-sys <69536330+Ammar-sys@users.noreply.github.com>
2021-01-17 11:40:23 +0100

    updating the randomness of the module

--- captcha_gen.py
+++ captcha_gen.py
@@ -1,5 +1,5 @@
 from PIL import ImageDraw, Image, ImageFont, ImageFilter
-import random, os, json
+import os, json, secrets
 
 def load_json():
     with open('./config.json', 'r') as f:
@@ -8,7 +8,7 @@ def load_json():
 
 def random_char(y):
     str1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    return ''.join(random.choice(str1) for _ in range(y))
+    return ''.join(secrets.choice(str1) for _ in range(y))
 
 font = [
     ImageFont.truetype("./fonts/gadugi.ttf", 19),
@@ -21,16 +21,16 @@ font = [
 curr_json = load_json()
 
 for _ in range(0, curr_json["number_to_generate"]):
-    fontsize = random.randint(10, 20)
-    img = Image.new('RGB', (100, 30), color=(random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)))
+    fontsize = secrets.randbelow(30)
+    img = Image.new('RGB', (100, 30), color=(secrets.randbelow(100), secrets.randbelow(100), secrets.randbelow(100)))
     d = ImageDraw.Draw(img)
 
-    for _ in range(0, random.randint(10, 30)):
-        d.line((random.randint(0, 100), random.randint(0, 100)) + img.size, fill=random.randint(0, 100))
+    for _ in range(0, secrets.randbelow(30)):
+        d.line((secrets.randbelow(100), secrets.randbelow(100)) + img.size, fill=secrets.randbelow(100))
 
     text = random_char(5)
-    d.text((24, 5), f"{text}", fill=(random.randint(1, 255), random.randint(1, 255), 0), font=random.choice(font))
-    imgblur = img.filter(ImageFilter.GaussianBlur(1.4))
+    d.text((24, 5), f"{text}", fill=(secrets.randbelow(255), secrets.randbelow(255), 0), font=secrets.choice(font))
+    imgblur = img.filter(ImageFilter.GaussianBlur(1.3))
     imgblur.save(fr'{curr_json["path"]}/{text}.png')
 
     dirListing = os.listdir(curr_json["path"])
