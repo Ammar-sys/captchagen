02ba1623aa94514966bda7f0b18d474b76c8dfc2
Ammar-sys <69536330+Ammar-sys@users.noreply.github.com>
2021-01-17 19:52:47 +0100

    MAKE IT NOT GENERATE GREEN IMAGES which mess up thte captchja ;(

--- captcha_gen.py
+++ captcha_gen.py
@@ -1,5 +1,5 @@
 from PIL import ImageDraw, Image, ImageFont, ImageFilter
-import os, json, secrets
+import os, json, secrets, random
 
 def load_json():
     with open('./config.json', 'r') as f:
@@ -12,25 +12,28 @@ def random_char(y):
 
 font = [
     ImageFont.truetype("./fonts/gadugi.ttf", 19),
-    ImageFont.truetype("./fonts/arial.ttf", 15),
-    ImageFont.truetype("./fonts/Chalkduster_400.ttf", 16),
-    ImageFont.truetype("./fonts/Chalkduster_400.ttf", 16),
-    ImageFont.truetype("./fonts/Chalkduster_400.ttf", 16)
+    ImageFont.truetype("./fonts/arial.ttf", 19),
+    ImageFont.truetype("./fonts/Chalkduster_400.ttf", 19),
+    ImageFont.truetype("./fonts/Chalkduster_400.ttf", 19),
+    ImageFont.truetype("./fonts/Chalkduster_400.ttf", 19)
 ]
 
 curr_json = load_json()
 
 for _ in range(0, curr_json["number_to_generate"]):
-    fontsize = secrets.randbelow(30)
-    img = Image.new('RGB', (100, 30), color=(secrets.randbelow(100), secrets.randbelow(100), secrets.randbelow(100)))
+    text_color = (random.randint(150, 255), 255, random.randint(150, 255))
+    image_color = (secrets.randbelow(255), random.randint(50, 125), secrets.randbelow(255))
+
+    fontsize = 16
+    img = Image.new('RGB', (100, 30), color=image_color)
     d = ImageDraw.Draw(img)
 
-    for _ in range(0, secrets.randbelow(30)):
+    for _ in range(0, secrets.randbelow(20)):
         d.line((secrets.randbelow(100), secrets.randbelow(100)) + img.size, fill=secrets.randbelow(100))
 
     text = random_char(5)
-    d.text((24, 5), f"{text}", fill=(secrets.randbelow(255), secrets.randbelow(255), 0), font=secrets.choice(font))
-    imgblur = img.filter(ImageFilter.GaussianBlur(1.3))
+    d.text((24, 5), f"{text}", fill=text_color, font=secrets.choice(font))
+    imgblur = img.filter(ImageFilter.GaussianBlur(1.1))
     imgblur.save(fr'{curr_json["path"]}/{text}.png')
 
     dirListing = os.listdir(curr_json["path"])
